0bedd454707f4d2a8cde5a777648aafd2259a44c
Killsoft <killsoft@localhost.localdomain>
2016-08-27 14:45:44 +0300

    Delete unused lines from python script

--- forumua.py
+++ forumua.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+
 from bs4 import BeautifulSoup
 import urllib
 
-#driver = webdriver.Firefox()
-#driver.get('http://forumua.org/forum/тематические-разделы/работа-и-трудоустройство/разовые-услуги-требуются')
 html = urllib.urlopen('http://forumua.org/forum/тематические-разделы/работа-и-трудоустройство/разовые-услуги-требуются') 
 soup = BeautifulSoup(html,'lxml')
 for tag in soup.find_all("td", class_="cell-topic js-cell-topic"):
@@ -14,11 +11,11 @@ for tag in soup.find_all("td", class_="cell-topic js-cell-topic"):
 		continue
 	else:
 		print disc.strip()
-	#print tag.attrs['href']
+	
 		href = tag.find('a', class_='topic-title js-topic-title')
+	
 		try:
 			print href.attrs['href']
 		except:
 			print 'Not found'
-#for tag in soup.find_all('div'):
-#	print tag
+
